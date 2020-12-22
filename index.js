a22affea9d2dee3ef302e05827a43d60fce70500
Bert De Block <bert.deblock@bagaar.be>
2020-12-22 19:37:09 +0100

    refactor: restructure code

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const recursiveReadDir = require('recursive-readdir');
+const flatToNested = require('./lib/flat-to-nested');
 
-const packageFile = fs.readFileSync('package.json');
-const packageJson = JSON.parse(packageFile);
-
-const isAddon = Boolean(packageJson['ember-addon']);
-const componentsDir = isAddon ? 'addon/components' : 'app/components';
-
-recursiveReadDir(componentsDir, function (error, componentFiles) {
-  if (error) {
-    console.log(error.message);
-  } else {
-    componentFiles.forEach(flatToNestedComponent);
-  }
-});
-
-function flatToNestedComponent(componentFile) {
-  if (isNestedComponent(componentFile)) {
-    return;
-  }
-
-  const [nestedComponentDir, nestedComponentExt] = componentFile.split('.');
-
-  const nestedComponentFile = `${nestedComponentDir}/index.${nestedComponentExt}`;
-
-  fs.mkdirSync(nestedComponentDir, { recursive: true });
-  fs.renameSync(componentFile, nestedComponentFile);
-}
-
-function isNestedComponent(componentFile) {
-  return componentFile.includes('/index.');
-}
+flatToNested(process.cwd());
