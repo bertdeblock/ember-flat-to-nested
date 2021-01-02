5a0d64f43fa29526206f983ba5aaa843a8b2e66a
Bert De Block <bert.deblock@bagaar.be>
2021-01-02 16:31:46 +0100

    test: run tests in parallel

--- test/test.js
+++ test/test.js
@@ -8,96 +8,96 @@ const flatToNested = require('../lib');
 
 test.beforeEach(cleanupOutput);
 
-test.serial('flat to nested inside a project', async function (t) {
+test('flat to nested inside a project', async function (t) {
   await copyBlueprint('project-flat');
-  await flatToNested(outputPath());
+  await flatToNested(outputPath('project-flat'));
 
-  t.false(await outputFileExists('app/components/foo.js'));
-  t.false(await outputFileExists('app/components/foo.hbs'));
-  t.false(await outputFileExists('app/components/foo.css'));
-  t.false(await outputFileExists('app/components/foo/bar.js'));
-  t.false(await outputFileExists('app/components/foo/bar.hbs'));
-  t.false(await outputFileExists('app/components/foo/bar.css'));
+  t.false(await outputFileExists('project-flat/app/components/foo.js'));
+  t.false(await outputFileExists('project-flat/app/components/foo.hbs'));
+  t.false(await outputFileExists('project-flat/app/components/foo.css'));
+  t.false(await outputFileExists('project-flat/app/components/foo/bar.js'));
+  t.false(await outputFileExists('project-flat/app/components/foo/bar.hbs'));
+  t.false(await outputFileExists('project-flat/app/components/foo/bar.css'));
 
-  t.true(await outputFileExists('app/components/foo/index.js'));
-  t.true(await outputFileExists('app/components/foo/index.hbs'));
-  t.true(await outputFileExists('app/components/foo/index.css'));
-  t.true(await outputFileExists('app/components/foo/bar/index.js'));
-  t.true(await outputFileExists('app/components/foo/bar/index.hbs'));
-  t.true(await outputFileExists('app/components/foo/bar/index.css'));
-  t.true(await outputFileExists('app/components/baz/index.js'));
-  t.true(await outputFileExists('app/components/baz/index.hbs'));
-  t.true(await outputFileExists('app/components/baz/index.css'));
+  t.true(await outputFileExists('project-flat/app/components/foo/index.js'));
+  t.true(await outputFileExists('project-flat/app/components/foo/index.hbs'));
+  t.true(await outputFileExists('project-flat/app/components/foo/index.css'));
+  t.true(await outputFileExists('project-flat/app/components/foo/bar/index.js'));
+  t.true(await outputFileExists('project-flat/app/components/foo/bar/index.hbs'));
+  t.true(await outputFileExists('project-flat/app/components/foo/bar/index.css'));
+  t.true(await outputFileExists('project-flat/app/components/baz/index.js'));
+  t.true(await outputFileExists('project-flat/app/components/baz/index.hbs'));
+  t.true(await outputFileExists('project-flat/app/components/baz/index.css'));
 });
 
-test.serial('nested to flat inside a project', async function (t) {
+test('nested to flat inside a project', async function (t) {
   await copyBlueprint('project-nested');
-  await flatToNested(outputPath(), { revert: true });
+  await flatToNested(outputPath('project-nested'), { revert: true });
 
-  t.true(await outputFileExists('app/components/foo.js'));
-  t.true(await outputFileExists('app/components/foo.hbs'));
-  t.true(await outputFileExists('app/components/foo.css'));
-  t.true(await outputFileExists('app/components/foo/bar.js'));
-  t.true(await outputFileExists('app/components/foo/bar.hbs'));
-  t.true(await outputFileExists('app/components/foo/bar.css'));
-  t.true(await outputFileExists('app/components/baz.js'));
-  t.true(await outputFileExists('app/components/baz.hbs'));
-  t.true(await outputFileExists('app/components/baz.css'));
+  t.true(await outputFileExists('project-nested/app/components/foo.js'));
+  t.true(await outputFileExists('project-nested/app/components/foo.hbs'));
+  t.true(await outputFileExists('project-nested/app/components/foo.css'));
+  t.true(await outputFileExists('project-nested/app/components/foo/bar.js'));
+  t.true(await outputFileExists('project-nested/app/components/foo/bar.hbs'));
+  t.true(await outputFileExists('project-nested/app/components/foo/bar.css'));
+  t.true(await outputFileExists('project-nested/app/components/baz.js'));
+  t.true(await outputFileExists('project-nested/app/components/baz.hbs'));
+  t.true(await outputFileExists('project-nested/app/components/baz.css'));
 
-  t.false(await outputFileExists('app/components/foo/index.js'));
-  t.false(await outputFileExists('app/components/foo/index.hbs'));
-  t.false(await outputFileExists('app/components/foo/index.css'));
-  t.false(await outputFileExists('app/components/foo/bar/index.js'));
-  t.false(await outputFileExists('app/components/foo/bar/index.hbs'));
-  t.false(await outputFileExists('app/components/foo/bar/index.css'));
+  t.false(await outputFileExists('project-nested/app/components/foo/index.js'));
+  t.false(await outputFileExists('project-nested/app/components/foo/index.hbs'));
+  t.false(await outputFileExists('project-nested/app/components/foo/index.css'));
+  t.false(await outputFileExists('project-nested/app/components/foo/bar/index.js'));
+  t.false(await outputFileExists('project-nested/app/components/foo/bar/index.hbs'));
+  t.false(await outputFileExists('project-nested/app/components/foo/bar/index.css'));
 });
 
-test.serial('flat to nested inside an addon', async function (t) {
+test('flat to nested inside an addon', async function (t) {
   await copyBlueprint('addon-flat');
-  await flatToNested(outputPath());
+  await flatToNested(outputPath('addon-flat'));
 
-  t.false(await outputFileExists('addon/components/foo.js'));
-  t.false(await outputFileExists('addon/components/foo.hbs'));
-  t.false(await outputFileExists('addon/components/foo.css'));
-  t.false(await outputFileExists('addon/components/foo/bar.js'));
-  t.false(await outputFileExists('addon/components/foo/bar.hbs'));
-  t.false(await outputFileExists('addon/components/foo/bar.css'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo.js'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo.hbs'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo.css'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo/bar.js'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo/bar.hbs'));
+  t.false(await outputFileExists('addon-flat/addon/components/foo/bar.css'));
 
-  t.true(await outputFileExists('addon/components/foo/index.js'));
-  t.true(await outputFileExists('addon/components/foo/index.hbs'));
-  t.true(await outputFileExists('addon/components/foo/index.css'));
-  t.true(await outputFileExists('addon/components/foo/bar/index.js'));
-  t.true(await outputFileExists('addon/components/foo/bar/index.hbs'));
-  t.true(await outputFileExists('addon/components/foo/bar/index.css'));
-  t.true(await outputFileExists('addon/components/baz/index.js'));
-  t.true(await outputFileExists('addon/components/baz/index.hbs'));
-  t.true(await outputFileExists('addon/components/baz/index.css'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/index.js'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/index.hbs'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/index.css'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/bar/index.js'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/bar/index.hbs'));
+  t.true(await outputFileExists('addon-flat/addon/components/foo/bar/index.css'));
+  t.true(await outputFileExists('addon-flat/addon/components/baz/index.js'));
+  t.true(await outputFileExists('addon-flat/addon/components/baz/index.hbs'));
+  t.true(await outputFileExists('addon-flat/addon/components/baz/index.css'));
 });
 
-test.serial('nested to flat inside an addon', async function (t) {
+test('nested to flat inside an addon', async function (t) {
   await copyBlueprint('addon-nested');
-  await flatToNested(outputPath(), { revert: true });
+  await flatToNested(outputPath('addon-nested'), { revert: true });
 
-  t.true(await outputFileExists('addon/components/foo.js'));
-  t.true(await outputFileExists('addon/components/foo.hbs'));
-  t.true(await outputFileExists('addon/components/foo.css'));
-  t.true(await outputFileExists('addon/components/foo/bar.js'));
-  t.true(await outputFileExists('addon/components/foo/bar.hbs'));
-  t.true(await outputFileExists('addon/components/foo/bar.css'));
-  t.true(await outputFileExists('addon/components/baz.js'));
-  t.true(await outputFileExists('addon/components/baz.hbs'));
-  t.true(await outputFileExists('addon/components/baz.css'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo.js'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo.hbs'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo.css'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo/bar.js'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo/bar.hbs'));
+  t.true(await outputFileExists('addon-nested/addon/components/foo/bar.css'));
+  t.true(await outputFileExists('addon-nested/addon/components/baz.js'));
+  t.true(await outputFileExists('addon-nested/addon/components/baz.hbs'));
+  t.true(await outputFileExists('addon-nested/addon/components/baz.css'));
 
-  t.false(await outputFileExists('addon/components/foo/index.js'));
-  t.false(await outputFileExists('addon/components/foo/index.hbs'));
-  t.false(await outputFileExists('addon/components/foo/index.css'));
-  t.false(await outputFileExists('addon/components/foo/bar/index.js'));
-  t.false(await outputFileExists('addon/components/foo/bar/index.hbs'));
-  t.false(await outputFileExists('addon/components/foo/bar/index.css'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/index.js'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/index.hbs'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/index.css'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/bar/index.js'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/bar/index.hbs'));
+  t.false(await outputFileExists('addon-nested/addon/components/foo/bar/index.css'));
 });
 
 function copyBlueprint(blueprintName) {
-  return recursiveCopy(blueprintPath(blueprintName), outputPath());
+  return recursiveCopy(blueprintPath(blueprintName), outputPath(blueprintName));
 }
 
 function outputFileExists(filePath) {
