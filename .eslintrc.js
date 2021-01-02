f7c7e199e7af992aa6ac7429709880ebe3831f9e
Bert De Block <bert.deblock@bagaar.be>
2021-01-02 16:51:07 +0100

    style: fix print width error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'script',
